c6edfba96a3ef204d657b0b5b086f3c4f56af919
dendi1981 <dendisutendi@gmail.com>
2026-09-12 21:32:28 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>caches.delete(n)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
